a5324f685c427d084c44414d039cd7dbf0a5b6d7
ijlaal001 <ijlaalakhtar@gmail.com>
2025-03-29 02:14:27 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Chocket</h1>
+            <a href="/login">Login</a> | <a href="/signup">Signup</a>
+        </div>
+    );
+};
+
+export default Home;
